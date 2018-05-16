691d986a9033c36635fd58f48266cd24abdfdfef
heindeep <heindeep15@googlemail.com>
2018-05-16 22:27:20 +0200

    Update Functions

--- main.js
+++ main.js
@@ -1,120 +1,180 @@
-/* jshint -W097 */// jshint strict:false
-/*jslint node: true */
 'use strict';
 
-// you have to require the utils module and call adapter function
-var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
-
-// you have to call the adapter function and pass a options object
-// name has to be set and has to be equal to adapters folder name and main file name excluding extension
-// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.btc_kurs.0
+var utils = require(__dirname + '/lib/utils');
+var request = require('request');
 var adapter = new utils.Adapter('btc_kurs');
 
-// is called when adapter shuts down - callback has to be called under any circumstances!
-adapter.on('unload', function (callback) {
-    try {
-        adapter.log.info('cleaned everything up...');
-        callback();
-    } catch (e) {
-        callback();
-    }
-});
-
-// is called if a subscribed object changes
-adapter.on('objectChange', function (id, obj) {
-    // Warning, obj can be null if it was deleted
-    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
-});
-
-// is called if a subscribed state changes
-adapter.on('stateChange', function (id, state) {
-    // Warning, state can be null if it was deleted
-    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
-
-    // you can use the ack flag to detect if it is status (true) or command (false)
-    if (state && !state.ack) {
-        adapter.log.info('ack is not set!');
-    }
-});
-
-// Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
-adapter.on('message', function (obj) {
-    if (typeof obj === 'object' && obj.message) {
-        if (obj.command === 'send') {
-            // e.g. send email or pushover or whatever
-            console.log('send command');
-
-            // Send response in callback if required
-            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-        }
-    }
-});
-
-// is called when databases are connected and adapter received configuration.
-// start here!
 adapter.on('ready', function () {
     main();
 });
 
+function number_format( number, length, sep, th_sep ) {
+	number = Math.round( number * Math.pow(10, length) ) / Math.pow(10, length);
+	var str_number = number + "";
+	var arr_int = str_number.split(".");
+	if(!arr_int[0]) arr_int[0] = "0";
+	if(!arr_int[1]) arr_int[1] = "";
+	if(arr_int[1].length < length){
+		var decimal = arr_int[1];
+		for(var i=arr_int[1].length + 1; i <= length; i++){  decimal += "0";  }
+		arr_int[1] = decimal;
+	}
+	if(th_sep !== '' && arr_int[0].length > 3){
+		var term = arr_int[0];
+		arr_int[0] = "";
+		for(var j = 3; j < term.length ; j+=3){
+			var extract = term.slice(term.length - j, term.length - j + 3);
+			arr_int[0] = th_sep + extract +  arr_int[0] + "";
+		}
+		var str_first = term.substr(0, (term.length % 3 === 0)?3:(term.length % 3));
+		arr_int[0] = str_first + arr_int[0];
+	}
+	return arr_int[0]+sep+arr_int[1];
+}
+
 function main() {
+	var exchange = adapter.config.exchange;
+	var btc = adapter.config.btc;
+	var eth = adapter.config.eth;
+	var ltc = adapter.config.ltc;
+	var bch = adapter.config.bch;
+    
+	if (exchange == "bitfinex") {
+		var url = "https://api.bitfinex.com/v1/pubticker/";
+	}
+		
+	if (btc == true) {
+		adapter.log.warn('Request BTC done');
+		request(
+			{
+				url: url + 'btcusd',
+				json: true
+			}, 
+			function(error, response, content) {
+				if (!error && response.statusCode == 200) {
+					let data = JSON.parse(JSON.stringify(content));
+					var json = JSON.stringify(data);
+					let jsonoutput = JSON.parse(JSON.stringify(json));
+					var output = eval("(function(){return " + jsonoutput + ";})()");
+					var ticker = parseFloat(output.last_price);
+					ticker = number_format(ticker,2,'.','');
+					adapter.setObject('BTC-USD_' + exchange, {
+						type: 'state',
+						common: {
+							name: 'BTC-USD_' + exchange,
+							type: 'string',
+							role: 'variable'
+						},
+						native: {}
+					});
+					adapter.log.info('BTC-USD_' + exchange + ': ' + ticker);	
+					adapter.setState('BTC-USD_' + exchange, {val: ticker, ack: true});
+				} else {
+					adapter.log.error(error);
+				}
+			}
+		);
+	}
+	if (eth == true) {
+		adapter.log.warn('Request ETH done');
+		request(
+			{
+				url: url + 'ethusd',
+				json: true
+			}, 
+			function(error, response, content) {
+				if (!error && response.statusCode == 200) {
+					let data = JSON.parse(JSON.stringify(content));
+					var json = JSON.stringify(data);
+					let jsonoutput = JSON.parse(JSON.stringify(json));
+					var output = eval("(function(){return " + jsonoutput + ";})()");
+					var ticker = parseFloat(output.last_price);
+					ticker = number_format(ticker,2,'.','');
+					adapter.setObject('ETH-USD_' + exchange, {
+						type: 'state',
+						common: {
+							name: 'ETH-USD_' + exchange,
+							type: 'string',
+							role: 'variable'
+						},
+						native: {}
+					});
+					adapter.log.info('ETH-USD_' + exchange + ': ' + ticker);	
+					adapter.setState('ETH-USD_' + exchange, {val: ticker, ack: true});
+				} else {
+					adapter.log.error(error);
+				}
+			}
+		);
+	}
 
+	if (ltc == true) {
+		adapter.log.warn('Request LTC done');
+		request(
+			{
+				url: url + 'ltcusd',
+				json: true
+			}, 
+			function(error, response, content) {
+				if (!error && response.statusCode == 200) {
+					let data = JSON.parse(JSON.stringify(content));
+					var json = JSON.stringify(data);
+					let jsonoutput = JSON.parse(JSON.stringify(json));
+					var output = eval("(function(){return " + jsonoutput + ";})()");
+					var ticker = parseFloat(output.last_price);
+					ticker = number_format(ticker,2,'.','');
+					adapter.setObject('LTC-USD_' + exchange, {
+						type: 'state',
+						common: {
+							name: 'LTC-USD_' + exchange,
+							type: 'string',
+							role: 'variable'
+						},
+						native: {}
+					});
+					adapter.log.info('LTC-USD_' + exchange + ': ' + ticker);	
+					adapter.setState('LTC-USD_' + exchange, {val: ticker, ack: true});
+				} else {
+					adapter.log.error(error);
+				}
+			}
+		);
+	}
 
-    adapter.log.info('Cryptocurrency: ' + adapter.config.cryptocurrency);
-    adapter.log.info('Exchange: ' + adapter.config.exchange);
-
-
-    /**
-     *
-     *      For every state in the system there has to be also an object of type state
-     *
-     *      Here a simple btc_kurs for a boolean variable named "testVariable"
-     *
-     *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-     *
-     */
-
-    adapter.setObject('testVariable', {
-        type: 'state',
-        common: {
-            name: 'testVariable',
-            type: 'boolean',
-            role: 'indicator'
-        },
-        native: {}
-    });
-
-    // in this btc_kurs all states changes inside the adapters namespace are subscribed
-    adapter.subscribeStates('*');
-
-
-    /**
-     *   setState examples
-     *
-     *   you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-     *
-     */
-
-    // the variable testVariable is set to true as command (ack=false)
-    adapter.setState('testVariable', true);
-
-    // same thing, but the value is flagged "ack"
-    // ack should be always set to true if the value is received from or acknowledged from the target system
-    adapter.setState('testVariable', {val: true, ack: true});
-
-    // same thing, but the state is deleted after 30s (getState will return null afterwards)
-    adapter.setState('testVariable', {val: true, ack: true, expire: 30});
-
-
-
-    // examples for the checkPassword/checkGroup functions
-    adapter.checkPassword('admin', 'iobroker', function (res) {
-        console.log('check user admin pw ioboker: ' + res);
-    });
-
-    adapter.checkGroup('admin', 'admin', function (res) {
-        console.log('check group user admin group admin: ' + res);
-    });
-
-
-
+	if (bch == true) {
+		adapter.log.warn('Request BCH done');
+		request(
+			{
+				url: url + 'bchusd',
+				json: true
+			}, 
+			function(error, response, content) {
+				if (!error && response.statusCode == 200) {
+					let data = JSON.parse(JSON.stringify(content));
+					var json = JSON.stringify(data);
+					let jsonoutput = JSON.parse(JSON.stringify(json));
+					var output = eval("(function(){return " + jsonoutput + ";})()");
+					var ticker = parseFloat(output.last_price);
+					ticker = number_format(ticker,2,'.','');
+					adapter.setObject('BCH-USD_' + exchange, {
+						type: 'state',
+						common: {
+							name: 'BCH-USD_' + exchange,
+							type: 'string',
+							role: 'variable'
+						},
+						native: {}
+					});
+					adapter.log.info('BCH-USD_' + exchange + ': ' + ticker);	
+					adapter.setState('BCH-USD_' + exchange, {val: ticker, ack: true});
+				} else {
+					adapter.log.error(error);
+				}
+			}
+		);
+	}
+	
+	setTimeout(function () {
+		adapter.stop();
+	}, 10000);
 }
